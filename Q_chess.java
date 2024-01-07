5281bfc2a61c33bf09c07085659c02d15caadedd
albertanon <153449512+albertanon@users.noreply.github.com>
2024-01-07 12:46:03 +0530

    Update Q_chess.java

--- Q_chess.java
+++ Q_chess.java
@@ -48,7 +48,7 @@ public class Q_chess {
         }
     }
     public static void main(String[] args) {
-        int n=5;
+        int n=10;
         char board[][]=new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
